e317a59440391c1dd747c2f29bf710c9c76757e5
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-02-07 13:01:46 +0545

    Operators

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Namespace1;
+{
+    class Program{
+        public static void Main(string[] args){
+            //Nothing Important
+            int a = 20, b = 10, res;
+            res = ((a > b) ?a : b);
+            Console.WriteLine("Result = " + res);
+            Console.ReadKey();
+        }
+    }
+}
