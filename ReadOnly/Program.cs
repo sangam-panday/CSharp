1a670839d02bb983fcd68096c16c66d776c611a0
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-02-07 18:30:35 +0545

    ReadOnly

---
+++ ReadOnly/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Readonly
+{
+    class Program
+    {   
+        readonly int x;
+        Program(int a)
+        {
+            x = a;
+        }
+        public void Display()
+        {
+            //x = 20; Creates error as it is readonly   
+            Console.WriteLine("Value of x is: " + x);
+        }
+        
+        static void Main()
+        {
+            Program p1 = new Program(10);
+            p1.Display();
+            Program p2 = new Program(20);
+            p2.Display();
+        }
+    }
+            // Create a new instance of the class
+}
