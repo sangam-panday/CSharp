e647352faf9f360ffd880a7b5114f14e9ba95314
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-02-07 18:24:01 +0545

    Temperature Convertor

---
+++ static/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Namespace
+{
+    class Program
+    {
+        //Runs before Main method
+        //Runs once per program
+        static Program()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+        //Runs when object is created
+        //Runs once per object
+        public Program()
+        {
+            Console.WriteLine("Instance Constructor");
+        }
+        int x = 10;
+        static int y = 20;
+        public void Method()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+        public int add(){
+            return x+y;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Method();
+            Console.WriteLine(p.add());
+        }
+    }
+}
---
+++ tempconv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace TempConv
+{
+    public static class TemperatureConvertor
+    {
+        public static double CelsiusToFahrenheit(string celsius)
+        {
+            double celsiusValue = double.Parse(celsius);
+            return (celsiusValue * 9 / 5) + 32;
+        }
+        public static double FahrenheitToCelsius(string fahrenheit)
+        {
+            double fahrenheitValue = double.Parse(fahrenheit);
+            return (fahrenheitValue - 32) * 5 / 9;
+        }
+    }
+    class TestTemperatureConvertor{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the temperature in Celsius: ");
+            string celsius = Console.ReadLine();
+            Console.WriteLine("Temperature in Fahrenheit: " + TemperatureConvertor.CelsiusToFahrenheit(celsius));
+            Console.WriteLine("Enter the temperature in Fahrenheit: ");
+            string fahrenheit = Console.ReadLine();
+            Console.WriteLine("Temperature in Celsius: " + TemperatureConvertor.FahrenheitToCelsius(fahrenheit));
+        }
+    }
+}
