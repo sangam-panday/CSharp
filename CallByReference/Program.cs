2433bf2b8a4831100b8fe06cb9c46c50786eee41
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-02-07 13:25:15 +0545

    Function and Call by reference

---
+++ CallByReference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Namespace1
+{
+    class Employee
+    {
+        public string Name = "";
+        public int Age;
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Name = "John";
+            emp.Age = 30;
+            Console.WriteLine("Before calling the method: Name = {0}, Age = {1}", emp.Name, emp.Age);
+            Change(emp);
+            Console.WriteLine("After calling the method: Name = {0}, Age = {1}", emp.Name, emp.Age);
+            UpdateName(/*ref*/ emp);
+            Console.WriteLine($"Emp1 Name = {emp.Name}");
+        }
+        public static void Change(/*ref*/ Employee emp)
+        {
+            emp.Name = "Mark";
+            emp.Age = 35;
+            Console.WriteLine("Inside the method: Name = {0}, Age = {1}", emp.Name, emp.Age);
+        }
+        public static void UpdateName(Employee Emp2)
+        {
+            Emp2 = null;
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Namespace1{
+    class Program{
+        static void Main(string[] args){
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            int sum = Add(a, b);
+            Console.WriteLine("Sum of Integers:");
+            Console.WriteLine($"Sum of {a} and {b} : " + sum);
+            sum = Add(a, b, c);
+            Console.WriteLine($"Sum of {a}, {b} and {c} : " + sum);
+            float x = 10.5f;
+            float y = 20.5f;
+            float fsum = Add(x, y);
+            Console.WriteLine("Sum of Floats:" );
+            Console.WriteLine($"Sum of {x} and {y} : " + fsum);
+        }
+        static int Add(int a, int b){
+            return a + b;
+        }
+        static int Add(int a, int b, int c){
+            return a + b + c;
+        }
+        static float Add(float a, float b){
+            return a + b;
+        }
+    }
+}
