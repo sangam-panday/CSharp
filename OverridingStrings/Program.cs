f9e15505b3358993d4ee35c4f5d9c813f0732099
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-02-08 09:40:25 +0545

    Overriding ToString()

---
+++ OverridingStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace stringoverriding{
+    class over{
+        static void Main(string[] args){
+            // Console.Write("Input a number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(num.ToString());
+            Employee e = new Employee();
+            e.Name = "John";
+            e.Age = 30;
+            Console.WriteLine(e.ToString());
+        }
+    }
+    class Employee{
+        public string Name;
+        public int Age;
+        public override string ToString(){
+            return "Name: " + Name + " Age: " + Age;
+        }
+    }
+}
